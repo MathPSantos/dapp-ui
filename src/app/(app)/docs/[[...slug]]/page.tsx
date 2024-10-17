b624f789d600e098693844231cb6b26d0071b397
MathPSantos <mathsantos.dev@gmail.com>
2024-10-17 10:31:07 -0300

    fix: build error

--- src/app/(app)/docs/[[...slug]]/page.tsx
+++ src/app/(app)/docs/[[...slug]]/page.tsx
@@ -2,12 +2,9 @@ import { promises as fs } from "fs";
 import { notFound } from "next/navigation";
 import path from "path";
 import { compileMDX } from "next-mdx-remote/rsc";
-import { ChevronLeftIcon, ChevronRightIcon } from "lucide-react";
+import { ChevronRightIcon } from "lucide-react";
 import { absoluteUrl, cn, removeFrontmatter } from "@/lib/utils";
 import Balancer from "react-wrap-balancer";
-import { NavItem, NavItemWithChildren, docsConfig } from "@/config/docs";
-import { buttonVariants } from "@/components/ui/button";
-import Link from "next/link";
 import { Metadata } from "next";
 import { siteConfig } from "@/config/site";
 import { mdxComponents } from "@/components/mdx-components";
@@ -21,6 +18,7 @@ import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import { getTableOfContents } from "@/lib/toc";
 import { ScrollArea } from "@/components/ui/scroll-area";
 import { DashboardTableOfContents } from "@/components/toc";
+import { DocsPager } from "@/components/pager";
 
 type DocPageProps = {
   params: {
@@ -211,55 +209,4 @@ export default async function DocPage({ params }: DocPageProps) {
       </div>
     </main>
   );
-}
-
-function DocsPager({ slug }: { slug: string }) {
-  const pager = getPagerForDoc(slug);
-
-  if (!pager) return null;
-
-  return (
-    <div className="flex flex-row items-center justify-between">
-      {pager?.prev?.href && (
-        <Link
-          href={pager.prev.href}
-          className={buttonVariants({ variant: "outline" })}
-        >
-          <ChevronLeftIcon className="mr-2 h-4 w-4" />
-          {pager.prev.title}
-        </Link>
-      )}
-      {pager?.next?.href && (
-        <Link
-          href={pager.next.href}
-          className={cn(buttonVariants({ variant: "outline" }), "ml-auto")}
-        >
-          {pager.next.title}
-          <ChevronRightIcon className="ml-2 h-4 w-4" />
-        </Link>
-      )}
-    </div>
-  );
-}
-
-function getPagerForDoc(slug: string) {
-  const flattenedLinks = [null, ...flatten(docsConfig.sidebarNav), null];
-  const activeIndex = flattenedLinks.findIndex((link) => slug === link?.href);
-  const prev = activeIndex !== 0 ? flattenedLinks[activeIndex - 1] : null;
-  const next =
-    activeIndex !== flattenedLinks.length - 1
-      ? flattenedLinks[activeIndex + 1]
-      : null;
-  return {
-    prev,
-    next,
-  };
-}
-
-export function flatten(links: NavItemWithChildren[]): NavItem[] {
-  return links
-    .reduce<NavItem[]>((flat, link) => {
-      return flat.concat(link.items?.length ? flatten(link.items) : link);
-    }, [])
-    .filter((link) => !link?.disabled);
 }
